f86e49cb3a2fe71754e96d7bb2388f68c2068c16
Lesly_hub <lesly.yareth150201@gmail.com>
2023-09-21 22:35:34 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/tarea5_200931/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarea5_180247
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// Creado por: Uriel Maldonado Cortez
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+import 'package:flutter/material.dart';
+import 'package:tarea5_180247/src/button.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyButton(),
+  ));
+}
+    
